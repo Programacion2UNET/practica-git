efd142965ebb96741936f5bfc7deef3cc03558ea
Miguel Angel Useche Castro <migueluseche@skatox.com>
2017-02-22 15:08:28 -0400

    añadiendo el hola mundo y ejemplo de enteros

---
+++ practica1/Practica.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace practica1
+{
+	public class Practica
+	{
+		public static void Main(string[] args)
+		{
+			Int32 num = 2017;
+			Console.WriteLine("Hola");
+			Console.WriteLine(num.ToString());
+		}
+	}
+}
