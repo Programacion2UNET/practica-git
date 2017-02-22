5a01d2983855e5fd8062761f27aabc3660723a2f
Pedro Jos�? Labrador P�?rez <pedro.labrador@labcomp.unet.edu.ve>
2017-02-22 14:36:57 -0430

    trying to upload something

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32 num = 2048;
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine(num.ToString());
+            Console.ReadLine();
+            
+        }
+    }
+}
