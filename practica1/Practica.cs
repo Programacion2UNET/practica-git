8d74f3cb60f255b8a09023bcef54e5ad636eed57
Miguel Angel Useche Castro <migueluseche@skatox.com>
2017-02-22 15:22:59 -0400

    Añadiendo comentario

--- practica1/Practica.cs
+++ practica1/Practica.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 namespace practica1
-{
+{	
 	public class Practica
 	{
 		public static void Main(string[] args)
 		{
+			//Probando que en C# el numero es un objeto
 			Int32 num = 2017;
 			Console.WriteLine("Hola");
 			Console.WriteLine(num.ToString());
