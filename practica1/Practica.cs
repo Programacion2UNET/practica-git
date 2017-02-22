0f6e12c542a42070531253c77d24aefb87623bd6
Armando Jose Ortuno Tremarias <armando.ortuno@labcomp.unet.edu.ve>
2017-02-22 15:35:55 -0430

    arreglando el codigo

--- practica1/Practica.cs
+++ practica1/Practica.cs
@@ -7,14 +7,9 @@ namespace practica1
 		{
 			//Probando que en C# el numero es un objeto
 			Int32 num = 2017;
-<<<<<<< HEAD
 			Console.WriteLine("Hola cambie el codigo");
-=======
 			Console.WriteLine("Hello World");
-
->>>>>>> c963b16746c28c0c982b238a7e08bfb435582b4d
 			Console.WriteLine(num.ToString());
-
             Console.ReadLine();
 		}
 	}
