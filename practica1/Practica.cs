bf466eb4f6ac6bb132f8ad7111303f380010b460
Armando <dobleuse@yahoo.com>
2017-02-22 22:55:24 -0400

    reestableciendo

--- practica1/Practica.cs
+++ practica1/Practica.cs
@@ -1,17 +1,18 @@
 ﻿using System;
-namespace practica1
-{	
-	public class Practica
-	{
-		public static void Main(string[] args)
-		{
+namespace practica1{
+	
+	public class Practica{
+
+		public static void Main(string[] args){
+
 			//Probando que en C# el numero es un objeto
-			Int32 num = 4F;
-            Console.WriteLine("Chiabe ayurame");
-            Console.WriteLine("https://i.ytimg.com/vi/Acu5BbVOcRU/maxresdefault.jpg");
-			Console.WriteLine("Hello World");
+			Int32 num = 2017;
+			Console.WriteLine("Hola");
 			Console.WriteLine(num.ToString());
-            Console.ReadLine();
+            		Console.ReadLine();
+	
 		}
+
 	}
+
 }
